9d1fb33350bbcc71792be8e1983d05d5763aa0c7
Anh Tuấn Nguyễn <tuananh251003@gmail.com>
2025-04-26 23:12:12 +0700

    NguyenAnhTuan_ChucNang_Quan_Ly_Thanh_Toan_UpdateChucNang_Thong_Tin_Tu_Van

--- NHOM20_DATN/NHOM20_DATN/Patient/Thong_Tin_Tu_Van.aspx.cs
+++ NHOM20_DATN/NHOM20_DATN/Patient/Thong_Tin_Tu_Van.aspx.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Data;
+using System.IO;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Web;
@@ -10,7 +13,7 @@ using System.Web.UI.WebControls;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using Newtonsoft.Json;
-using MimeKit;
+using System.Net;
 using MailKit.Security;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;

da396eea957292875991e1ffe56cc1bd7eb927fc
Nguyen Van Viet <nguyenvanviet8@dtu.edu.vn>
2025-05-06 12:11:09 +0700

    NguyenVanViet_UpdateChucNangHoTroDatLichKham

--- NHOM20_DATN/NHOM20_DATN/Consultant/Ho_Tro_Dat_Lich_Kham.aspx.designer.cs
+++ NHOM20_DATN/NHOM20_DATN/Consultant/Ho_Tro_Dat_Lich_Kham.aspx.designer.cs
@@ -33,6 +33,24 @@ namespace NHOM20_DATN.Consultant
         protected global::System.Web.UI.WebControls.Button btnTimKiemBN;
 
         /// <summary>
+        /// ScriptManager1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager ScriptManager1;
+
+        /// <summary>
+        /// UpdatePanelMain control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel UpdatePanelMain;
+
+        /// <summary>
         /// ddlChuyenKhoa control.
         /// </summary>
         /// <remarks>

abc1f5aa54391c8c876730f71c081cc1f93c0d6f
PhamVietHau11 <phamviethau@dtu.edu.vn>
2025-05-12 09:00:21 +0700

    PhamVietHau_Update_Masterpage_Mangerment

--- NHOM20_DATN/NHOM20_DATN/MasterPage/Managerment_MasterPage.Master.cs
+++ NHOM20_DATN/NHOM20_DATN/MasterPage/Managerment_MasterPage.Master.cs
@@ -11,18 +11,18 @@ namespace NHOM20_DATN.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            //if (!IsPostBack)
-            //{
-            //    if ((string)Session["Role"] == null || (string)Session["Role"] == "")
-            //    {
-            //        Response.Redirect("/Dang_Nhap.aspx");
-            //    }
-            //    else if ((string)Session["Role"] != "QuanLy")
-            //    {
-            //        Response.Redirect("~/Error_forbidden.html");
-            //    }
+            if (!IsPostBack)
+            {
+                if ((string)Session["Role"] == null || (string)Session["Role"] == "")
+                {
+                    Response.Redirect("/Dang_Nhap.aspx");
+                }
+                else if ((string)Session["Role"] != "QuanLy")
+                {
+                    Response.Redirect("/Error_forbidden.html");
+                }
 
-            //}
+            }
         }
         
         //Đăng xuất
